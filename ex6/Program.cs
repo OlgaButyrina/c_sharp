ca584634605ec37f27324c29a919fb99712013c7
Olga <olgabutyrina2020@gmail.com>
2023-01-29 22:29:33 +0800

    ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Напишите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{ 
+    Console.WriteLine ("УРА, это же МАША!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.Write (username);
+    Console.Write ("!");
+}
