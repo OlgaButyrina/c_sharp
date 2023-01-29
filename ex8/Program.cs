4160af461c9aaaf575ac7be9b362a7a39cffe674
Olga <olgabutyrina2020@gmail.com>
2023-01-30 00:11:05 +0800

    ex8

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
-int xa = 40, ya = 2,
-xb = 1, yb = 20,
-xc = 80, yc = 20;
+int xa = 40, ya = 1,
+xb = 1, yb = 30,
+xc = 80, yc = 30;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
